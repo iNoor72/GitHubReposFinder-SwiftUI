092342c7b53a390f4aa8bc102357e327f5978e09
Noor El-Din Walid <52334140+iNoor72@users.noreply.github.com>
2023-07-07 23:06:41 +0300

    Initial Commit

---
+++ GitHubReposFinder-SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitHubReposFinder-SwiftUI
+//
+//  Created by Nooreldin Walid on 07/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubReposFinder-SwiftUI/GitHubReposFinder_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubReposFinder_SwiftUIApp.swift
+//  GitHubReposFinder-SwiftUI
+//
+//  Created by Nooreldin Walid on 07/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubReposFinder_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
