12082ba40fdfaf225d175a95a46b703413cd410f
Noor El-Din Walid <52334140+iNoor72@users.noreply.github.com>
2023-07-07 23:08:08 +0300

    initial commit.

--- GitHubReposFinder-SwiftUI/GitHubReposFinder_SwiftUIApp.swift
+++ GitHubReposFinder-SwiftUI/GitHubReposFinder_SwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct GitHubReposFinder_SwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
